619e2f8f3237cbabca06eab37d43ea017299a8e3
Shivang Sinha <shivangsinha.88@outlook.com>
2025-11-09 00:54:44 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,16 @@
+public class pattern{
+    public static void main(String[] args) {
+        for (int i = 1; i < 5;){
+            for (int j = 0; j <= i; j++){
+                if( j % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+           
+                
+        }
+        System.out.println();
+    }
+    }
+}
